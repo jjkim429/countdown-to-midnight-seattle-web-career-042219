091c5a273fd84c50cc3e4201d56ec770f319d192
Jennifer Kim <jjkim429@gmail.com>
2019-04-05 22:29:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(number)
-  while number >= 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(n)
+  while n >= 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
   end
   "HAPPY NEW YEAR!"
 end
